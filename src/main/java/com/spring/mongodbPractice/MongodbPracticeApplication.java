1f60b3e8aa26bdbef89ea7940ddfe6f46fdc8fb7
raihanuddin561 <raihanuddin561@gmail.com>
2022-11-06 21:53:14 +0600

    Code base of mongodb project

---
+++ src/main/java/com/spring/mongodbPractice/MongodbPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mongodbPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/mongodbPractice/MongodbPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mongodbPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
