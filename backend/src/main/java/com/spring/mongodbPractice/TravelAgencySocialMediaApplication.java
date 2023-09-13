b53a441e399d1bf28410f30c5f829c97bf1667db
raihanuddin561 <raihanuddin561@gmail.com>
2023-09-13 06:28:21 +0600

    Renamed project

--- backend/src/main/java/com/spring/mongodbPractice/TravelAgencySocialMediaApplication.java
+++ backend/src/main/java/com/spring/mongodbPractice/TravelAgencySocialMediaApplication.java
@@ -9,10 +9,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class MongodbPracticeApplication {
+public class TravelAgencySocialMediaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MongodbPracticeApplication.class, args);
+		SpringApplication.run(TravelAgencySocialMediaApplication.class, args);
 	}
 
 
